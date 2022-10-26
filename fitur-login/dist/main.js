746a9c81aa1137b4ba69da7858f4f37fd6c4b9a0
Syahdad <116540147+Syahdad@users.noreply.github.com>
2022-10-26 14:07:06 +0700

    Upload file fitur login

---
+++ fitur-login/dist/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    
+    $('#slider-hero').owlCarousel({
+        loop: true,
+        nav: true,
+        // mouseDrag: false,
+        items: 1,
+        dots: false,
+        margin: 0,
+        navText: [
+            '<i class="fas fa-angle-left" aria-hidden="true"><i>',
+            '<i class="fas fa-angle-right" aria-hidden="true"><i>'
+        ],
+        navContainer: '#slider-hero-nav',
+    });
+    
+    $('#sarana-mq-slider').owlCarousel({
+        loop: true,
+        nav: true,
+        // mouseDrag: false,
+        items: 2,
+        dots: false,
+        margin: 5,
+        navText: [
+            '<i class="fas fa-angle-left" aria-hidden="true"><i>',
+            '<i class="fas fa-angle-right" aria-hidden="true"><i>'
+        ],
+        navContainer: '#slider-tools-1',
+    });
+
+    $('#alumni-slider').owlCarousel({
+        loop: true,
+        nav: true,
+        // mouseDrag: false,
+        items: 2,
+        dots: false,
+        margin: 20,
+        navText: [
+            '<i class="fas fa-angle-left" aria-hidden="true"><i>',
+            '<i class="fas fa-angle-right" aria-hidden="true"><i>'
+        ],
+        navContainer: '#slider-tools-2',
+    });
+
+});
